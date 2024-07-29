782d5576f3a5d6cf86fb8411bd4f86b9cb8962e7
Ye Zhenrong <3471626272@qq.com>
2024-07-29 16:05:49 +0800

    Fix Bugs

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -6,7 +6,9 @@ package DebugExercise;
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
+        float t = top;
+        float b = bottom;
+        float quotient = t / b;
         int result = Math.round(quotient);
         return result;
     }
