ad4245fd4d6b378c4eacd8bddfe6e73ed9314fb0
Ye Zhenrong <3471626272@qq.com>
2024-09-12 21:42:56 +0800

    Update

--- proj2/gitlet-design.md
+++ proj2/gitlet-design.md
@@ -2,34 +2,48 @@
 
 **Name**: RobinYe
 
+This Gitlet Design Document is referred
+by [Capers Design Document](https://sp21.datastructur.es/materials/proj/proj2/capers-example).
+
 ## Classes and Data Structures
 
 ### Main Class
 
+This is the entry point to our program. It takes in arguments from the command line and based on the command (the first
+element of the `args` array) calls the corresponding command in `Repository` which will actually execute the logic of
+the command. It also validates the arguments based on the command to ensure that enough arguments were passed in.
+
 #### Fields
 
-1. Field 1
-2. Field 2
+This class has no fields and hence no associated state: it simply validates arguments and defers the execution to the
+`Repository` class.
+
+### Repository Class
+
+This is where the main logic of our program will live. This file will handle all of the actual gitlet commands by
+reading/writing from/to the correct file, setting up persistence, and additional error checking.
+
+It will also be responsible for setting up all persistence within gitlet. This includes creating the `.gitlet` folder as
+well as the folder and file where we store all File objects and the corresponding infomation.
+
+This class defers all `Commit` specific logic to the `Commit` class: for example, instead of having the `Repository`
+class
+handle `Commit` serialization and deserialization, we have the `Commit` class contain the logic for that.
+
+#### Fields
+
+1. `static final File CWD = new File(System.getProperty("user.dir"))`
+2. `static final File CAPERS_FOLDER = Utils.join(CWD, ".gitlet")`
 
 ### Commit Class
 
 #### Fields
 
-1. Field 1
-2. Field 2
-
-### Repository Class
-
-#### Fields
-
-1. Field 1
-2. Field 2
+1.
 
 ### Utils Class
 
 ## Algorithms
-
-// TODO
 
 ## Persistence
 
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,15 +1,17 @@
 package gitlet;
 
 import java.io.File;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
 
-/** Represents a gitlet repository.
+/**
+ * Represents a gitlet repository.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author RobinYe
+ * @author RobinYe
  */
 public class Repository {
     /**
@@ -20,10 +22,76 @@ public class Repository {
      * variable is used. We've provided two examples for you.
      */
 
-    /** The current working directory. */
+    /**
+     * The current working directory.
+     */
     public static final File CWD = new File(System.getProperty("user.dir"));
-    /** The .gitlet directory. */
+    /**
+     * The .gitlet directory.
+     */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     /* TODO: fill in the rest of this class. */
+    public static void init() {
+        if (GITLET_DIR.exists()) {
+            GITLET_DIR.mkdir();
+        } else {
+            System.err.println("A Gitlet version-control system already exists in the current directory.");
+        }
+        // TODO
+    }
+
+    public static void add(String file) {
+
+    }
+
+    public static void commit(String message) {
+        // TODO
+        // Invoke Commit class
+    }
+
+    public static void rm(String file) {
+        // TODO
+        System.err.println("No reason to remove the file.");
+    }
+
+    public static void log() {
+        // TODO
+    }
+
+    public static void global-
+
+    log() {
+        // TODO
+        // A useful method in Utils.
+    }
+
+    public static void find() {
+        // TODO
+    }
+
+    public static void status() {
+        // TODO
+    }
+
+    public static void checkout() {
+        // TODO
+    }
+
+
+    public static void branch(String branch) {
+        // TODO
+    }
+
+    public static void rm-branch(String branch) {
+        // TODO
+    }
+
+    public static void reset(String commitID) {
+        // TODO
+    }
+
+    public static void merge(String branch) {
+        // TODO
+    }
 }
